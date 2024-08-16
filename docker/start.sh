301c5f2143e75928edcf9b53f955b722b1a992d3
scottshireman <129428073+scottshireman@users.noreply.github.com>
2024-08-16 16:54:38 -0400

    Update start.sh

--- docker/start.sh
+++ docker/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cat /welcome.txt
+#cat /welcome.txt
 export PYTHONUNBUFFERED=1
 
 if [[ ! -f "v2-inference-v.yaml" ]]; then
@@ -9,22 +9,22 @@ fi
 mkdir -p logs input
 
 # RunPod SSH
-if [[ -v "PUBLIC_KEY" ]] && [[ ! -d "${HOME}/.ssh" ]]
-then
-    pushd $HOME
-    mkdir -p .ssh
-    echo ${PUBLIC_KEY} > .ssh/authorized_keys
-    chmod -R 700 .ssh
-    popd
-    service ssh start
-fi
+#if [[ -v "PUBLIC_KEY" ]] && [[ ! -d "${HOME}/.ssh" ]]
+#then
+#    pushd $HOME
+#    mkdir -p .ssh
+#    echo ${PUBLIC_KEY} > .ssh/authorized_keys
+#    chmod -R 700 .ssh
+#    popd
+#    service ssh start
+#fi
 
 tensorboard --logdir /workspace/EveryDream2trainer/logs --host 0.0.0.0 &
 
 # RunPod JupyterLab
 if [[ $JUPYTER_PASSWORD ]]
 then
-    jupyter nbextension enable --py widgetsnbextension
+#    jupyter nbextension enable --py widgetsnbextension
     jupyter labextension disable "@jupyterlab/apputils-extension:announcements"
     jupyter lab --allow-root --no-browser --port=8888 --ip=* --ServerApp.terminado_settings='{"shell_command":["/bin/bash"]}' --ServerApp.token=$JUPYTER_PASSWORD --ServerApp.allow_origin=* --ServerApp.preferred_dir=/workspace/EveryDream2trainer
 else
